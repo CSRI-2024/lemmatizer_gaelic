e4306bf9b1cabf16bbe1bc562c441c9dbb267e22
Oskar <evhoskar5@gmail.com>
2025-05-20 00:55:49 -0500

    Initial lemmatizer python file

---
+++ lemmatizer.py
@@ -0,0 +1,26 @@
+
+import json
+
+# Load irregular dictionary
+with open("irregular_dict.json", "r") as f:
+    irregulars = json.load(f)
+
+def lemmatize(word):
+    if word in irregulars:
+        return irregulars[word]
+    elif word.endswith("ean"):
+        return word[:-3]
+    elif word.endswith("an"):
+        return word[:-2]
+    elif word.endswith("achan"):
+        return word[:-5] + "ach"
+    else:
+        return word
+
+
+# Test it with mock tokens
+with open("mock_tokens.txt", "r") as f:
+    tokens = [line.strip() for line in f.readlines()]
+
+for token in tokens:
+    print(f"{token} → {lemmatize(token)}")
